45016e9b164893d2e0e6f12046ddd2d3d24e1451
Ian Tester <imroykun@gmail.com>
2014-03-15 02:09:15 +1100

    onion-fec: Shorten armv* machine names to simply 'arm'.
    And create the library directory before trying to copy the files into it.

--- onion-fec/src/csrc/build.sh
+++ onion-fec/src/csrc/build.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 JAVA_HOME="$(dirname "$(dirname "$(readlink -f "$(which javac)")")")"
-DIST_DIR=../../bin/lib/"$(uname -s | tr A-Z a-z)"-"$(uname -m)"
+DIST_DIR=../../bin/lib/"$(uname -s | tr A-Z a-z)"-"$(uname -m | sed -e 's|armv[0-9].*|arm|')"
 
 set -x
 
 JAVA_HOME="$JAVA_HOME" make
+mkdir -p "$DIST_DIR"
 cp libfec*.so "$DIST_DIR"
